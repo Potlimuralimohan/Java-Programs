ada450411ed93e26832f359343b326c30dd49380
MuraliMohan <126702504+Potlimuralimohan@users.noreply.github.com>
2024-06-06 13:22:35 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,15 @@
+class Boolean{
+    public static void main(String args[])
+    {
+        boolean num =true;
+        while(num)
+        {
+            System.out.println("murali");
+            break;
+        }
+        if(num)
+        {
+            System.out.println("Murali");
+        }
+    }
+}
