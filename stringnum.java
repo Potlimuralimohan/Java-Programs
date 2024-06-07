ffa7044736d5fce6f469af46b0c7a7c9101cf7b8
potlimuralimohan <potlimuralimohan123@gmail.com>
2024-06-07 14:46:28 +0530

    Added stringnum.java

---
+++ stringnum.java
@@ -0,0 +1,18 @@
+/**
+ * stringnum
+ */
+public class stringnum {
+    public static void main(String[] args) 
+    {
+        String str1="murali";
+        //String str1=new String("murali");
+        // str1.concat("mohan");
+        //System.out.println(str1+"mohan");
+        int y=20;
+        String val=str1+y;
+        System.out.println(val);
+
+        int randnum=(int)(Math.random());
+        System.out.println(randnum);
+    }
+}
